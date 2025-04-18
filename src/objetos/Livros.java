fe10898d2edb11d658de84a36dae6c080b87bf55
VictorHS82 <hugoscatamberlo@gmail.com>
2025-04-18 16:19:59 -0300

    prepraração para inserção de funcionalidade

---
+++ src/objetos/Livros.java
@@ -0,0 +1,33 @@
+package objetos;
+
+public class Livros {
+	private String cod_livro;
+	private String titulo;
+	private String autor;
+	private String descricao;
+	private int precoUnid;
+	private int publicacao;
+	private int isbn;
+	private String genero;
+	private String idioma;
+	private String formato;
+	private int paginas;
+	private float peso;
+	private String acabamento;
+	private String corte;
+	private int quantidade;
+	
+	private static void cadastrar() {
+		
+	}
+	
+	private static void modificarLivro() {
+		
+	}
+	
+	private static void verificarestoque() {
+		
+	}
+	
+
+}
