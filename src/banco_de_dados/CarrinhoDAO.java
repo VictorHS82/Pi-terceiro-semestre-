a90ea27c3931799229e13d9c6d0c1601a2573b32
VictorHS82 <hugoscatamberlo@gmail.com>
2025-06-05 00:41:25 -0300

    Classe DAO de cadastro

--- src/banco_de_dados/CarrinhoDAO.java
+++ src/banco_de_dados/CarrinhoDAO.java
@@ -7,6 +7,9 @@ import java.util.List;
 import objetos.Carrinho;
 import objetos.Item_carrinho;
 
+/**
+ * Classe DAO para o controle de carrinho, e itens do carrinho
+ */
 public class CarrinhoDAO {
 
 	private final BD bd = new BD();
@@ -16,21 +19,27 @@ public class CarrinhoDAO {
 			+ "VALUES(?,?)";
 	
 	private static String adicionarItemAoCarrinho = "INSERT INTO item_carrinho"
-			+"(cod_item_carrinho, cod_carrinho, cod_livro, nome_livro, quantidade, preco)"
-			+ "VALUES(?,?,?,?,?)";
+			+"(cod_item_carrinho, cod_carrinho, cod_livro, nome_livro, quantidade, preco_livro)"
+			+ "VALUES(?,?,?,?,?,?)";
 	
 	private static String pegarItensDoCarrinho = "SELECT * FROM item_carrinho"
-			+"WHERE id_carrinho = ?";
+			+" WHERE cod_carrinho = ?";
 	
 	private static String excluirItemDoCarrinho = "DELETE FROM item_carrinho"
-			+"WHERE id_tem_carrinho = ?";
+			+" WHERE cod_item_carrinho = ?";
 	
-	private static String excluirCarrinho = "DELETE * FROM carrinho"
-			+"WHERE id_carrinho = ?";
+	private static String excluirCarrinho = "DELETE FROM carrinho"
+			+" WHERE cod_carrinho = ?";
 	
 	public CarrinhoDAO() {
 		bd.getConnection();
 	}
+	
+	/**
+	 * Classe para criar um carrinho
+	 * @param carrinho
+	 * @return boolean
+	 */
 
 	public boolean criarCarrinho(Carrinho carrinho) {
 		String sql = criarCarrinho;
@@ -50,6 +59,11 @@ public class CarrinhoDAO {
 		}
 	}
 	
+	/**
+	 * Classe para deletar um carrinho
+	 * @param cod_carrinho
+	 * @return boolean
+	 */
 	public boolean deletarCarrinho(int cod_carrinho) {
 		String sql = excluirCarrinho;
         try (PreparedStatement stmt = bd.connection.prepareStatement(sql)) {
@@ -65,6 +79,12 @@ public class CarrinhoDAO {
             bd.close();
         }
     }
+	
+	/**
+	 * Classe para criar um item_carrinho
+	 * @param item_carrinho
+	 * @return boolean
+	 */
 	
 	public boolean post(Item_carrinho item_carrinho) {
 		String sql = adicionarItemAoCarrinho;
@@ -88,12 +108,19 @@ public class CarrinhoDAO {
 	}	
 		}
 
+	/**
+	 * Lista todos os itens_pedido vinculados ao carrinho
+	 * Do usuário
+	 * @param cod_carrinho
+	 * @return lista de itens do carrinho
+	 */
    public List<Item_carrinho> get(int cod_carrinho) {
 	   List<Item_carrinho> lista = new ArrayList<>();
 	   String sql = pegarItensDoCarrinho;
 	   
-	   try(PreparedStatement preparedstatement = bd.connection.prepareStatement(sql);
-			   ResultSet rs = preparedstatement.executeQuery()) {
+	   try(PreparedStatement preparedstatement = bd.connection.prepareStatement(sql)){
+		   preparedstatement.setInt(1, cod_carrinho);   
+		   ResultSet rs = preparedstatement.executeQuery();
 
 				while (rs.next()) {
 				 Item_carrinho item = new Item_carrinho(
@@ -102,7 +129,7 @@ public class CarrinhoDAO {
 					rs.getInt("cod_livro"),
 					rs.getString("nome_livro"),
 					rs.getInt("quantidade"),
-					rs.getFloat("preco")
+					rs.getFloat("preco_livro")
 					);
 				 lista.add(item);
 				}
@@ -114,6 +141,11 @@ public class CarrinhoDAO {
 	   return lista;
    }
    
+   /**
+    * Exclui um item_pedido do carrinho
+    * @param cod_item_carrinho
+    * @return boolean
+    */
    public boolean delete(int cod_item_carrinho) {
 	   String sql = excluirItemDoCarrinho;
 
